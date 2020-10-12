8395054648a3515323ca5ec4cbac39f937ad160b
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2020-10-12 18:44:37 +0200

    Add files via upload

---
+++ precache-manifest.a8ac71a4bf5af454c5698b0f2a90ac8f.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94a4fe3128fcc85e63dacca432cadb90",
+    "url": "./index.html"
+  },
+  {
+    "revision": "568719cb7627000fafdc",
+    "url": "./static/css/2.3774684b.chunk.css"
+  },
+  {
+    "revision": "217dea005da985120f08",
+    "url": "./static/css/main.684c1bea.chunk.css"
+  },
+  {
+    "revision": "568719cb7627000fafdc",
+    "url": "./static/js/2.a4af53f5.chunk.js"
+  },
+  {
+    "revision": "50d4f725d684ab4042c68f8faf4f936f",
+    "url": "./static/js/2.a4af53f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "217dea005da985120f08",
+    "url": "./static/js/main.d33d5762.chunk.js"
+  },
+  {
+    "revision": "f0c4570e4af9df885133",
+    "url": "./static/js/runtime-main.61903be0.js"
+  },
+  {
+    "revision": "5adacb3ce339e33b6968a39406fb3526",
+    "url": "./static/media/1-photo.5adacb3c.png"
+  },
+  {
+    "revision": "f9d5152c3b34f84b3903fdb4ee9356d3",
+    "url": "./static/media/2-photo.f9d5152c.png"
+  },
+  {
+    "revision": "d36b76328501037301d5b8f5dd84f2ad",
+    "url": "./static/media/3-photo.d36b7632.png"
+  },
+  {
+    "revision": "40465e9df813ffa493c7fedc4837e87b",
+    "url": "./static/media/4-photo.40465e9d.png"
+  },
+  {
+    "revision": "e32a1fc679df16e1447be61a2f333900",
+    "url": "./static/media/envelope.e32a1fc6.svg"
+  },
+  {
+    "revision": "512d0df6d711cc30896c99a9c9485b1d",
+    "url": "./static/media/group-12-copy.512d0df6.svg"
+  },
+  {
+    "revision": "31e446f9e923cd0d1fda1b689bb8d095",
+    "url": "./static/media/kolor_high.31e446f9.png"
+  },
+  {
+    "revision": "6d3cde4f6d191f4bacd397b8cde2873b",
+    "url": "./static/media/kolor_low.6d3cde4f.png"
+  },
+  {
+    "revision": "023e7fa5cc7956d366e376486409f36c",
+    "url": "./static/media/kolor_medium.023e7fa5.png"
+  },
+  {
+    "revision": "f3498df3e95bf1ea0f6a459f52087fb0",
+    "url": "./static/media/kolor_very_high.f3498df3.png"
+  },
+  {
+    "revision": "5945391d8f429e0a6477ad76a3df8a44",
+    "url": "./static/media/kolor_very_low.5945391d.png"
+  },
+  {
+    "revision": "0d947d1d66367f156b10025ca227674b",
+    "url": "./static/media/kropki-baner-kompresja.0d947d1d.svg"
+  },
+  {
+    "revision": "353f31d69c58ac58c51f16a0bbcb7778",
+    "url": "./static/media/kropki-dol-kompresja.353f31d6.svg"
+  },
+  {
+    "revision": "1a3796ba15033ac43aecf168374ddc82",
+    "url": "./static/media/map-marker.1a3796ba.svg"
+  },
+  {
+    "revision": "f86ffa07794defd29b9b5f8ce28c3ed4",
+    "url": "./static/media/phone.f86ffa07.svg"
+  }
+]);
